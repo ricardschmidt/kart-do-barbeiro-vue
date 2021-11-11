3733450bd7a39460d1c5901faf804f892753e72a
Ricardo <ricardschmidt@gmail.com>
2021-11-11 17:45:15 -0300

    alterando texto na home

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -27,12 +27,12 @@
       <div class="container text-center">
         <div class="row justify-content-md-center">
           <div class="col-md-12 col-lg-8">
-            <h2 class="title">Completed with examples</h2>
+            <h2 class="title">Venha fazer parte desse Campeonato</h2>
             <h5 class="description">
-              The kit comes with three pre-built pages to help you get started
-              faster. You can change the text and images and you're good to go.
-              More importantly, looking at them will give you a picture of what
-              you can built with this powerful Bootstrap 4 ui kit.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+			  facilis. Cumque molestias adipisci odio error ex maiores optio?
+			  Provident reprehenderit alias officia? Possimus aperiam facilis
+			  sapiente minus, rem explicabo doloribus?
             </h5>
           </div>
         </div>
