8c0221c83cd9811b7cfb61d6eb029ba56db956a6
Ricardo <ricardschmidt@gmail.com>
2022-02-14 15:01:07 -0300

    Atualização de msg de erro

--- src/pages/UploadXlsx.vue
+++ src/pages/UploadXlsx.vue
@@ -176,7 +176,7 @@ export default {
 			}).catch(error => {
 				this.loading = false
 				this.error = true
-				this.errorMsg = error.response.data.error.messageUser
+				this.errorMsg = error.response.data.error.messageUser ? error.response.data.error.messageUser : error.response.data.error.message
 			});
 
 			this.clearFieds()
